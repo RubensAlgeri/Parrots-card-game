22ac9eb93c4bfb7635344e1b792233542a154c43
Rubens Algeri <rubens_zys@hotmail.com>
2022-02-06 20:18:13 -0300

    fazer o jogo reiniciar pedindo a qtde de cartas novamente

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,4 @@
-let numeroCartas = prompt("Escolha um número par entre 4 e 14 para jogar");
+let numeroCartas;
 let validacao = 0;
 let cartasContadas = [];
 let primeiraCarta;
@@ -10,162 +10,142 @@ let vencer = 0;
 let numeroJogadas = 0;
 let respostaSatisfatoria = 0;
 let intervalo;
+const relogio = document.querySelector(".relogio");
 
-while (validacao < 1) {
-    numeroCartas = prompt("Escolha um número par entre 4 e 14 para jogar");
-    if (numeroCartas > 14 || numeroCartas < 4 || numeroCartas % 2 !== 0) {
-        numeroCartas = prompt("Escolha um número par entre 4 e 14 para jogar");
-    } else {
-        validacao = 1;
-    }
-}
 const cartas = [
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/bobrossparrot.gif" alt="bobrossparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/bobrossparrot.gif" alt="bobrossparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/explodyparrot.gif" alt="explodyparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/explodyparrot.gif" alt="explodyparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/fiestaparrot.gif" alt="fiestaparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/fiestaparrot.gif" alt="fiestaparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/metalparrot.gif" alt="metalparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/metalparrot.gif" alt="metalparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/revertitparrot.gif" alt="revertitparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/revertitparrot.gif" alt="revertitparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/tripletsparrot.gif" alt="tripletsparrot">
-</figure>
-</div>`,
-
-    `<div onclick="cartaClicada(this) " class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/tripletsparrot.gif" alt="tripletsparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/unicornparrot.gif" alt="unicornparrot">
-</figure>
-</div>`,
-
-    `<div class="carta" data-identifier="card">
-<div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
-    <img src="/imagens/front.png" alt="Frente da Carta">
-</div>
-<figure class="verso face" data-identifier="front-face">
-    <img src="/gifs/unicornparrot.gif" alt="unicornparrot">
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/bobrossparrot.gif" alt="bobrossparrot">
     </figure>
-</div>`
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/bobrossparrot.gif" alt="bobrossparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/explodyparrot.gif" alt="explodyparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/explodyparrot.gif" alt="explodyparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/fiestaparrot.gif" alt="fiestaparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/fiestaparrot.gif" alt="fiestaparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/metalparrot.gif" alt="metalparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/metalparrot.gif" alt="metalparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/revertitparrot.gif" alt="revertitparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/revertitparrot.gif" alt="revertitparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/tripletsparrot.gif" alt="tripletsparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/tripletsparrot.gif" alt="tripletsparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/unicornparrot.gif" alt="unicornparrot">
+    </figure>
+</article>`,
+
+`<article class="carta" data-identifier="card">
+    <div onclick="cartaClicada(this)" class="frente face" data-identifier="back-face">
+        <img src="/imagens/front.png" alt="Frente da Carta">
+    </div>
+    <figure class="verso face" data-identifier="front-face">
+        <img src="/gifs/unicornparrot.gif" alt="unicornparrot">
+        </figure>
+<articlev>`
 ];
 
+iniciar();
 
-function comparador() {
-    return Math.random() - 0.5;
-}
-for (let i = 0; i < numeroCartas; i++) {
-    cartasContadas.push(cartas[i]);
-    cartasContadas.sort(comparador);
-}
-
-for (let contador = 0; contador < numeroCartas; contador++) {
-    document.querySelector("main").innerHTML += cartasContadas[contador];
-}
-
-function virarCarta(virar) {
-    virar.parentNode.querySelector(".verso").style.transform = "rotateY(0deg)";
-    virar.parentNode.querySelector(".frente").style.transform = "rotateY(-180deg)";
-}
+destribuirCartas();
 
 function cartaClicada(cartaSelecionada) {
+
     respostaSatisfatoria = 0;
 
     if (contagem === 0) {
@@ -176,11 +156,14 @@ function cartaClicada(cartaSelecionada) {
     virarCarta(cartaSelecionada);
 
     if (contagem === 1) {
+
         carta1 = cartaSelecionada.parentNode;
         primeiraCarta = cartaSelecionada.parentNode.querySelector("figure img").alt;
         contagem = 2;
         numeroJogadas++;
+
     } else if (contagem === 2) {
+
         contagem = 3;
         document.querySelectorAll(".frente").onclick = "";
         carta2 = cartaSelecionada.parentNode;
@@ -189,9 +172,12 @@ function cartaClicada(cartaSelecionada) {
         selecionarCartas();
 
         if (primeiraCarta !== segundaCarta) {
+
             setTimeout(desvirar, 1000);
             setTimeout(deselecionarCartas, 1000);
+
         } else {
+
             deselecionarCartas();
             vencer++;
             contagem = 1;
@@ -201,29 +187,35 @@ function cartaClicada(cartaSelecionada) {
                 alert(`Você ganhou em ${numeroJogadas} jogadas!`);
 
                 while (respostaSatisfatoria < 1) {
+
                     let resposta = prompt("Quer jogar novamente? s/n");
 
                     if (resposta === 's') {
+
                         relogio.innerHTML = 0;
                         validacao = 0;
                         respostaSatisfatoria = 2;
                         contagem = 0;
                         vencer = 0;
                         numeroJogadas = 0;
+                        cartasContadas = [];
+
                         clearInterval(intervalo);
-                        const deClasses = document.querySelectorAll(".verso")
+
+                        const deClasses = document.querySelectorAll("article")
 
                         deClasses.forEach(element => {
-                            element.style.transform = "rotateY(180deg)";
+                            element.remove();
                         });
-                        const dClasses = document.querySelectorAll(".frente")
 
-                        dClasses.forEach(element => {
-                            element.style.transform = "rotateY(0deg)";
-                        });
+                        iniciar();
+                        destribuirCartas();
+
                     } else if (resposta === 'n') {
+
                         alert("Obrigado por jogar, volte sempre!!!")
                         respostaSatisfatoria = 2;
+
                     } else {
                         alert("Por favor digite apenas s para sim ou n para não.");
                     }
@@ -232,6 +224,36 @@ function cartaClicada(cartaSelecionada) {
         }
     }
 }
+function iniciar() {
+    while (validacao < 1) {
+        numeroCartas = prompt("Escolha um número par entre 4 e 14 para jogar");
+        if (numeroCartas > 14 || numeroCartas < 4 || numeroCartas % 2 !== 0) {
+            numeroCartas = prompt("Escolha um número par entre 4 e 14 para jogar");
+        } else {
+            validacao = 1;
+        }
+    }
+}
+
+function comparador() {
+    return Math.random() - 0.5;
+}
+function destribuirCartas() {
+    for (let i = 0; i < numeroCartas; i++) {
+        cartasContadas.push(cartas[i]);
+        cartasContadas.sort(comparador);
+    }
+
+    for (let contador = 0; contador < numeroCartas; contador++) {
+        document.querySelector("main").innerHTML += cartasContadas[contador];
+    }
+}
+
+function virarCarta(virar) {
+    virar.parentNode.querySelector(".verso").style.transform = "rotateY(0deg)";
+    virar.parentNode.querySelector(".frente").style.transform = "rotateY(-180deg)";
+}
+
 function desvirar() {
     carta1.querySelector(".verso").style.transform = "rotateY(180deg)";
     carta1.querySelector(".frente").style.transform = "rotateY(0deg)";
@@ -239,7 +261,6 @@ function desvirar() {
     carta2.querySelector(".frente").style.transform = "rotateY(0deg)";
     contagem = 1;
 }
-const relogio = document.querySelector(".relogio");
 
 function aumentarContagem() {
     relogio.innerHTML = parseInt(relogio.innerHTML) + 1;
